8afd8dfea2e19468506bc4b1361e09203257d669
Keita Tsukamoto <t.k.columbus1012@gmail.com>
2019-01-30 15:54:42 +0900

    Functinos

--- src/Game.kt
+++ src/Game.kt
@@ -4,21 +4,43 @@ fun main(args: Array<String>) {
     val isBlessed = true
     val isImmortal = false
 
-    // Aura
-    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
-    val auraColor = if (auraVisible) "GREEN" else "BLUE"
-    println(auraColor)
+    val auraColor = auraColor(isBlessed, healthPoints, isImmortal)
+    val healthStatus = formatHealthStatus(healthPoints, isBlessed)
 
-    val healthStatus = when(healthPoints) {
-        100 -> "is in excellent condition!"
-        in 90..99 -> "has a few scratches."
-        in 75..89 -> if (isBlessed) {
-                        "has some minor wounds but is healing quite quickly."
-                     } else {
-                        "has some minor wounds."
-                     }
-        in 15..74 -> "looks pretty hurt."
-        else -> "is in awful condition!"
-    }
+    // Player status
+    printPlayerStatus(auraColor, isBlessed, name, healthStatus)
+    castFireball()
+}
+
+private fun printPlayerStatus(
+    auraColor: String,
+    isBlessed: Boolean,
+    name: String,
+    healthStatus: String
+) {
+    println(
+        "(Aura: $auraColor) " +
+                "(Blessed: ${if (isBlessed) "YES" else "NO"})"
+    )
     println("$name $healthStatus")
 }
+
+private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean): String {
+    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
+    val auraColor = if (auraVisible) "GREEN" else "BLUE"
+    return auraColor
+}
+
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean) = when (healthPoints) {
+    100 -> "is in excellent condition!"
+    in 90..99 -> "has a few scratches."
+    in 75..89 -> if (isBlessed) {
+        "has some minor wounds but is healing quite quickly."
+    } else {
+        "has some minor wounds."
+    }
+    in 15..74 -> "looks pretty hurt."
+    else -> "is in awful condition!"
+}
+
+private fun castFireball(numFireballs: Int = 2) = println("A glass of Fireball springs into existence. (x$numFireballs)")
